c0a6703d49bf93826ea567953c24de5c6d75346f
525santushevich <sophiaantushevich@icloud.com>
2024-02-02 11:52:11 -0500

    final commit all done on replit nothing else works

---
+++ script.js
@@ -0,0 +1,24 @@
+function show1822() {
+    document.getElementById('words').innerText= 'The Greek War of Independence, which began in 1821, was a culmination of years of simmering discontent and a resurgence of Hellenic identity. The catalyst for the uprising was the call for autonomy and the rejection of Ottoman oppression, coupled with inspiration drawn from the Age of Enlightenment and other nationalist movements in Europe. The Greeks, led by charismatic figures such as Theodoros Kolokotronis and Alexandros Ypsilantis, engaged in a protracted struggle against the Ottoman Empire, enduring both triumphs and setbacks. International support played a pivotal role, with Philhellenes and foreign powers, particularly Britain, France, and Russia, providing aid to the Greek cause. The Greek War of Independence stands as a testament to the enduring spirit of national identity and the pursuit of freedom.'
+    document.getElementById('lol').src = 'greece.jpg'
+}
+
+function show1911() {
+    document.getElementById('words').innerText= 'The Triangle Shirtwaist Factory fire, a tragic event in American history, occurred on March 25, 1911, in New York City. The fire broke out in the Triangle Shirtwaist Factory, a garment sweatshop located in the Asch Building. The facility was notorious for its unsafe working conditions, overcrowded workspace, and locked exit doors. As the fire quickly spread, the workers, mostly young immigrant women, found themselves trapped. The inadequate fire safety measures and locked doors resulted in the loss of 146 lives. This devastating incident became a catalyst for significant labor reforms and the establishment of workplace safety regulations in the United States. The Triangle Shirtwaist Factory fire highlighted the need for better working conditions and contributed to the growth of the labor movement, leading to improved safety standards and the recognition of the rights of the workers.'
+    document.getElementById('lol').src = 'triangle.jpg'
+}
+
+function show1918() {
+    document.getElementById('words').innerText= 'Claude Debussy, born on August 22, 1862, in Saint-Germain-en-Laye, France, was a pioneering French composer whose work had a profound impact on the development of 20th-century music. His innovative use of non-traditional scales, harmonic ambiguity, and rich orchestrations marked a departure from the era of Romantic conventions, influencing generations of composers. His music reflects a fascination with nature and an exploration of new soundscapes, showcasing a unique blend of lyricism and modernism. Claude Debussy passed away on March 25, 1918, leaving behind a legacy that continues to inspire and shape the world of classical music.'
+    document.getElementById('lol').src = 'claude.jpg'
+}
+
+function show1942() {
+  document.getElementById('words').innerText= 'Aretha Franklin, often referred to as the "Queen of Soul," was an iconic American singer, songwriter, and pianist. Born on March 25, 1942, in Memphis, Tennessee, her remarkable career spanned several decades and left an indelible mark on the music industry. Known for her powerful and soulful voice, she seamlessly blended gospel, R&B, and pop, earning her widespread acclaim. Aretha Franklin passed away on August 16, 2018, but her soulful voice and timeless music remain an integral part of American musical history.'
+  document.getElementById('lol').src = 'aretha1.jpg'
+}
+
+function show1947() {
+    document.getElementById('words').innerText= 'Elton John, born Reginald Kenneth Dwight on March 25, 1947, is a legendary British singer, songwriter, and pianist. Renowned for his flamboyant stage presence and distinctive voice, Elton John has left an indelible mark on the world of pop and rock music. With a career spanning over five decades, he has produced an extensive catalog of hits, including classics like "Your Song," "Rocket Man," and "Tiny Dancer." Knighted in 1998, his enduring legacy and contribution to the world of music and charity continue to be celebrated globally.'
+    document.getElementById('lol').src = 'elton.jpg'
+}
